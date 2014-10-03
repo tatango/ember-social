4d1aa3ede8323bed2b2bc631b0ff5a7f2ce14894
Denis Nazarov <zvezda@gmail.com>
2014-10-03 17:35:29 -0400

    Don’t default to document title or url, remove example with no params.

--- app/components/email-share.js
+++ app/components/email-share.js
@@ -2,18 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tracking: null, // optional injection
-  subject: null,
+  subject: '',
   body: null,
-  url: null,
+  url: '',
   bodyText: function(){
     var body = this.get('body');
     return body ? body + '\n\n' : '';
   }.property('body'),
   href: function(){
-    var subjectText = this.get('subject') || document.title;
-    var subject = encodeURIComponent(subjectText);
-    var shareUrl = this.get('shareUrl') || document.URL;
-    var body = encodeURIComponent(this.get('bodyText') + shareUrl);
+    var subject = encodeURIComponent(this.get('subject'));
+    var body = encodeURIComponent(this.get('bodyText') + this.get('url'));
     return  "mailto:?subject="+ subject + "&body="+ body;
   }.property('subject', 'bodyText', 'shareUrl'),
   tagName: 'a',
