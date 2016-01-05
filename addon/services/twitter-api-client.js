15742e5d9b7db49e045f5093c72b51fffca19b0f
Luis Coimbra <lcoimbra@riotgames.com>
2016-01-05 18:44:38 -0200

    added a returning value to twttr variable when it detects script tag already inserted into the page

--- addon/services/twitter-api-client.js
+++ addon/services/twitter-api-client.js
@@ -18,7 +18,7 @@ export default Ember.Object.extend({
         window.twttr = (function(d, s, id) {
           var js, fjs = d.getElementsByTagName(s)[0],
             t = window.twttr || {};
-          if (d.getElementById(id)) {return;}
+          if (d.getElementById(id)) { return window.twttr; }
           js = d.createElement(s);
           js.id = id;
           js.src = "https://platform.twitter.com/widgets.js";
